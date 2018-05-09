6146460ab5a144a23a02b99c5cc61d32d14a98f4
xp <xp@CETYSDAM011>
2018-05-09 17:16:19 +0200

    Haciendo el ejercicio lemario

--- src/ejerciciolemario/EjercicioLemario.java
+++ src/ejerciciolemario/EjercicioLemario.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,16 +22,46 @@ public class EjercicioLemario {
 
     public void cargaFicheroLemario(){
         File fichero = new File("src/ejerciciolemario/lemario-20101017.txt");
+        HashMap<String, Boolean> lemario = new HashMap();
         try {
             FileReader fr = new FileReader(fichero);
             BufferedReader br = new BufferedReader(fr);
-            String linea; 
+            String linea ; 
             int cont = 0; 
+            String palabra = "hola";
+            
+            
             while ( (linea = br.readLine()) != null){
-                //System.out.println(linea);
-                cont++;
+                lemario.put(linea, Boolean.TRUE);
+                if(lemario.get("h") != null){
+                for(int i = 0; i < lemario.size(); i++){
+                    if(lemario.containsKey(linea.charAt(i)));
+                
+                }
+                System.out.println(linea);
             }
-            System.out.println(cont);
+                else{
+                    System.out.println("Error");
+                }
+                
+                
+                
+                cont++;
+                
+                
+               
+            }
+            /*
+            	for ( int i = 0; i < cadena.length(); i++ ) {
+			if ( h.containsKey ( cadena.charAt ( i ) ) ) {
+				h.put ( cadena.charAt ( i ), (int)h.get ( cadena.charAt ( i ) ) + 1 );
+			} else {
+				h.put ( cadena.charAt ( i ), 1 );
+			}
+		}
+            */
+            
+            //System.out.println(cont);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(EjercicioLemario.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
