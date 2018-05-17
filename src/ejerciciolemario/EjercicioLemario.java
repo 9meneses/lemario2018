4401ad0a78ee51bcc8a06d39f182c415ca79365f
Roberto <Roberto@DESKTOP-LRNG8TT>
2018-05-17 20:28:38 +0200

    Ejercicio realizado en el que comprueba si la escalera de palabras esta en el lemario y si realmente la escalera de palabras es una escalera de palabras

--- src/ejerciciolemario/EjercicioLemario.java
+++ src/ejerciciolemario/EjercicioLemario.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,61 +20,80 @@ import java.util.logging.Logger;
  * @author xp
  */
 public class EjercicioLemario {
+     HashMap<String, Boolean> lemario = new HashMap<>();
 
-    public void cargaFicheroLemario(){
+     public void cargaFicheroLemario(){
+    
         File fichero = new File("src/ejerciciolemario/lemario-20101017.txt");
-        HashMap<String, Boolean> lemario = new HashMap();
-        try {
+        try  {
             FileReader fr = new FileReader(fichero);
             BufferedReader br = new BufferedReader(fr);
-            String linea ; 
-            int cont = 0; 
-            String palabra = "hola";
+            String linea;
             
-            
-            while ( (linea = br.readLine()) != null){
-                lemario.put(linea, Boolean.TRUE);
-                if(lemario.get("h") != null){
-                for(int i = 0; i < lemario.size(); i++){
-                    if(lemario.containsKey(linea.charAt(i)));
-                
-                }
-                System.out.println(linea);
-            }
-                else{
-                    System.out.println("Error");
-                }
-                
-                
-                
-                cont++;
-                
-                
+            while ((linea = br.readLine()) != null){
                
-            }
-            /*
-            	for ( int i = 0; i < cadena.length(); i++ ) {
-			if ( h.containsKey ( cadena.charAt ( i ) ) ) {
-				h.put ( cadena.charAt ( i ), (int)h.get ( cadena.charAt ( i ) ) + 1 );
-			} else {
-				h.put ( cadena.charAt ( i ), 1 );
-			}
-		}
-            */
+                lemario.put(linea, true);
+            } 
             
-            //System.out.println(cont);
-        } catch (FileNotFoundException ex) {
+        } 
+        catch (FileNotFoundException ex) {
             Logger.getLogger(EjercicioLemario.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
+        } 
+        catch (IOException ex) {
             Logger.getLogger(EjercicioLemario.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
+      public boolean buscar(String palabra){
+    
+        if(lemario.containsKey(palabra)){
+            return true;
+        }
+        return false;
+    }
+      public boolean escaleraDePalabras(ArrayList<String> escalera){
+        
+	boolean palabra = true;
+        int contador;
+	for (int i = 0; i < escalera.size() - 1; i++) {	
+            palabra = buscar(escalera.get(i));
+            if(!palabra){
+                return false;
+            }
+            
+            contador = 0;
+	    if (escalera.get(i + 1).length() == escalera.get(i).length())  {		
+		for (int j = 0; j < escalera.get(i).length(); j++) {
+                    if(escalera.get(i).charAt(j) != escalera.get(i + 1).charAt(j)){
+                        contador++;
+                    }
+                    if(contador > 1){ //si tiene ya dos palabras distintas devuelve false
+                    
+                        palabra = false;
+                    }
+                    if(contador == 1){
+                        palabra =  true;
+                    }
+                    
+                    
+                        
+                    
+		}
+	    } 
+	}
+	return palabra;
+    }
+        
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new EjercicioLemario().cargaFicheroLemario();
+          ArrayList<String> listaPalabras = new ArrayList();
+        EjercicioLemario ejercicio = new EjercicioLemario();
+        ejercicio.cargaFicheroLemario();
+        listaPalabras.add("pato");
+        listaPalabras.add("pata");
+        System.out.println(ejercicio.escaleraDePalabras(listaPalabras));
     }
     
 }
